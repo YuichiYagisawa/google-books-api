4b7b32eef213a74307450e8be11d57d86827b7e3
Yuichi Yagisawa <yuichi9915@gmail.com>
2020-07-16 17:36:28 +0900

    add inc-serch js and html

--- app/resources/js/serch.js
+++ app/resources/js/serch.js
@@ -2,30 +2,39 @@ $(window).on('load', function() {
   var
     preFunc = null,
     preInput = '',
-    input = '',
-    ajaxSerch = function(input) {
-      $.ajax({
-        url: "api/serch",
-        type: "GET",
-        headers: {
-          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        },
-        data: ("keyword=" + input),
-        // success:function(data){
-        //   alert(data);
-        // },error:function(){ 
-        //     alert("error!!!!");
-        // }
-      }).then(
-        function ($books){
-          var books = $books;
-          // console.log(books);
+    input = ''
 
-          $.each(books, function(index, elem){
-            $('<li></li>').append(elem.title).appendTo('#book_list');
+  function builtHTML(data) {
+    var html = `
+      <li>${data.title}</li>
+    `
+    $('#book_list').append(html);
+  };
+
+
+  function ajaxSerch(input) {
+    $.ajax({
+      url: "api/serch",
+      type: "GET",
+      headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+      },
+      data: ("keyword=" + input),
+    })
+      .done(function($books){
+        $('#book_list').empty();
+        var data = $books
+        console.log(data);
+        $('#book_list').empty();
+        if (data.length !== 0) {
+          data.forEach(function(data) { //dataは配列型に格納されているのでEach文で回す
+            builtHTML(data)
           });
+        } else {
+          $('#book_list').html('該当する本はありません');
+        }
       });
-    };
+  };
   
   $('#inc_serch').on('keyup', function(){
     input = $.trim($('#inc_serch').val());
